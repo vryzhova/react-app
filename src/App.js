37e06a30d16ae158aa9cc9d5662777353790f013
Valeria Ryzhova <ryzhova_valieriia@mail.ru>
2022-07-03 19:11:44 +0300

    create marvel site

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import { Chart } from "react-google-charts";
 import './App.css';
-export const data = [
-  ["Task", "Hours per Day"],
-  ["Work", 11],
-  ["Eat", 2],
-  ["Commute", 2],
-  ["Watch TV", 2],
-  ["Sleep", 7],
-];
+import './components/marvelCard/marvel.css'
+import Card from './components/marvelCard/Card'
+import heroes from './components/marvelInfo'
 
-export const options = {
-  title: "My Daily Activities",
-  is3D: true,
-};
 
 function App() {
-  return (
-    <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"100%"}
-      height={"600px"}
-    />
+  return ( 
+    <div className='App'>
+      {
+        heroes.map((hero)=>
+        <Card name={hero.name} img = {hero.img} universe = {hero.universe} alter={hero.alter} 
+        friends ={hero.friends} superpowers ={hero.superpowers}
+        />
+        )
+      }
+    </div>
   );
 }
 
